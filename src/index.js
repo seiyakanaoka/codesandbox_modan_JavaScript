c1e0a7b0357d8fc78c23323c831064c25aecd73e
seiyakanaoka <78468016+seiyakanaoka@users.noreply.github.com>
2021-08-24 08:02:58 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -38,3 +38,30 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+// テンプレート文字列
+
+// const name = "じゃけぇ";
+// const age = 28;
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// アロー関数
+
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です。"));
+
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です。"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
