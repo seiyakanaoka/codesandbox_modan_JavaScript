bdae2440d4d9824eeb711889f92a90e63bc6ab8c
seiyakanaoka <78468016+seiyakanaoka@users.noreply.github.com>
2021-08-24 08:43:48 +0900

    map&filterについて

--- src/index.js
+++ src/index.js
@@ -122,3 +122,32 @@
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
+
+// map&filter
+
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if(name === "じゃけぇ") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
